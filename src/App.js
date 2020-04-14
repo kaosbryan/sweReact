77e0978316c0513c64262e0c40d08904e67c4d5a
Kaostft135 <kaosbryan99@gmail.com>
2020-04-14 14:53:11 -0400

    changes to app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,6 @@ import React from 'react';
 import Review from "./Review";
 import './App.css';
 
-const publicURL = 'https://swe432tomcat.herokuapp.com';
-export const getLocationUrlData = () => {
-  return {
-      url:
-          process.env.NODE_ENV === 'production'?
-          publicURL
-          :`${window.location.origin}`,
-      hash: `${window.location.hash}`
-  };
-};
-export const servicePath ='/a7';
-
 function App(){
   return(
      <div>
